55c4c08b47b649cc67428f614923164aedd4d9c2
M1571 <94639639+M1571@users.noreply.github.com>
2022-02-21 01:20:41 +0000

    Start js

---
+++ main.js
@@ -0,0 +1,25 @@
+// console.log('prova');
+
+const selectLevels = document.getElementById('levels');
+const playButton = document.getElementById('play');
+
+// 
+const startGame = () => {
+
+    console.log('Game Started')
+
+        // 
+        const mode = selectLevels.value
+        switch( mode ) {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+        }
+}
+
+playButton.addEventListener('click', startGame);
